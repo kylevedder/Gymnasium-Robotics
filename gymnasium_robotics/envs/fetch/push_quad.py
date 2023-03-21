65381d41e566eef19a9759ea5cc9453b31374f47
Edward Hu <husedward@gmail.com>
2023-03-20 20:48:00 -0400

    push quad hard task

--- gymnasium_robotics/envs/fetch/push_quad.py
+++ gymnasium_robotics/envs/fetch/push_quad.py
@@ -130,3 +130,152 @@ class MujocoFetchPushQuadEnv(MujocoFetchEnv, EzPickle):
             self.render()
 
         return obs, {}
+
+# push into all four quadrants
+class MujocoFetchPushHardQuadEnv(MujocoFetchEnv, EzPickle):
+    def __init__(self, camera_names=None, reward_type="sparse", action_space_type="object", **kwargs):
+        initial_qpos = {
+            "robot0:slide0": 0.405,
+            "robot0:slide1": 0.48,
+            "robot0:slide2": 0.0,
+            "object0:joint": [1.25, 0.53, 0.4, 1.0, 0.0, 0.0, 0.0],
+        }
+        self.camera_names = camera_names
+        MujocoFetchEnv.__init__(
+            self,
+            model_path=MODEL_XML_PATH,
+            has_object=True,
+            block_gripper=True,
+            n_substeps=20,
+            gripper_extra_height=0.0,
+            target_in_the_air=False,
+            target_offset=0.0,
+            obj_range=0.03,
+            target_range=0.15,
+            distance_threshold=0.05,
+            initial_qpos=initial_qpos,
+            reward_type=reward_type,
+            **kwargs,
+        )
+        self.action_space_type = action_space_type
+        # consists of images and proprioception.
+        _obs_space = {}
+        if isinstance(camera_names, list) and len(camera_names) > 0:
+            for c in camera_names:
+                _obs_space[c] = spaces.Box(
+                    0, 255, shape=(self.height, self.width, 3), dtype="uint8"
+                )
+        self.observation_space = spaces.Dict(_obs_space)
+        EzPickle.__init__(self, camera_names=camera_names, image_size=32, reward_type=reward_type, **kwargs)
+        
+        self.goal_idx = 0
+        q1_goal = np.array([1.1, 0.95, 0.42])
+        q2_goal = np.array([1.1, 0.55, 0.42])
+        q3_goal = np.array([1.5, 0.55, 0.42])
+        q4_goal = np.array([1.5, 0.95, 0.42])
+        self.goals = [q1_goal, q2_goal, q3_goal, q4_goal]
+
+    def _sample_goal(self):
+        goal = self.goals[self.goal_idx]
+        return goal.copy()
+
+    def _reset_sim(self):
+        self.data.time = self.initial_time
+        self.data.qpos[:] = np.copy(self.initial_qpos)
+        self.data.qvel[:] = np.copy(self.initial_qvel)
+        if self.model.na != 0:
+            self.data.act[:] = None
+
+        # Randomize start position of object.
+        if self.has_object:
+            # object_xpos = [1.2, 0.85] + self.np_random.uniform(-self.obj_range, self.obj_range, size=2)
+            object_xpos = [1.2, 0.85]
+            object_qpos = self._utils.get_joint_qpos(
+                self.model, self.data, "object0:joint"
+            )
+            assert object_qpos.shape == (7,)
+            object_qpos[:2] = object_xpos
+            self._utils.set_joint_qpos(
+                self.model, self.data, "object0:joint", object_qpos
+            )
+
+        self._mujoco.mj_forward(self.model, self.data)
+        return True
+
+    def _get_obs(self):
+        obs = {}
+        if hasattr(self, "mujoco_renderer"):
+            self._render_callback()
+            for c in self.camera_names:
+                img = self.mujoco_renderer.render(self.render_mode, camera_name=c)
+                obs[c] = img
+        else:
+            # BaseRobotEnv has called _get_obs to determine observation space dims but mujoco renderer has not been initialized yet.
+            # in this case, return an obs dict with arbitrary values for each ey
+            # since observation space will be overwritten later.
+            img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+            obs["achieved_goal"] = obs["observation"] = img
+        return obs
+
+    def step(self, action):
+        if np.array(action).shape != self.action_space.shape:
+            raise ValueError("Action dimension mismatch")
+
+        if self.action_space_type == "object":
+            # todo: make this xy control, and rescale to 5cm increments.
+            action = np.clip(action, -1, 1)
+            action = action * 0.05
+            obj0_pos = self._utils.get_site_xpos(self.model, self.data, "object0")
+            obj0_new_pos = obj0_pos + np.array([*action[:2], 0])
+            obj0_qpos = np.array(self._utils.get_joint_qpos(self.model, self.data, "object0:joint"))
+            obj0_qpos[:2] = obj0_new_pos[:2]
+            self._utils.set_joint_qpos(
+                self.model, self.data, "object0:joint", obj0_qpos
+            )
+            self._mujoco.mj_forward(self.model, self.data)
+            self._mujoco_step(action)
+            self._step_callback()
+        else:
+            action = np.clip(action, self.action_space.low, self.action_space.high)
+            self._set_action(action)
+
+            self._mujoco_step(action)
+
+            self._step_callback()
+
+        if self.render_mode == "human":
+            self.render()
+        obs = self._get_obs()
+
+        obj0_pos = self._utils.get_site_xpos(self.model, self.data, "object0")
+        info = {
+            "is_success": self._is_success(obj0_pos, self.goal),
+        }
+
+        terminated = self.compute_terminated(obj0_pos, self.goal, info)
+        truncated = self.compute_truncated(obj0_pos, self.goal, info)
+
+        reward = self.compute_reward(obj0_pos, self.goal, info)
+        if info["is_success"] and self.goal_idx < len(self.goals) - 1:
+            self.goal_idx += 1
+            self.goal = self.goals[self.goal_idx]
+
+        return obs, reward, terminated, truncated, info
+
+    def reset(
+        self,
+        *,
+        seed = None,
+        options = None,
+    ):
+        # removed super.reset call
+        self.goal_idx = 0
+        did_reset_sim = False
+        while not did_reset_sim:
+            did_reset_sim = self._reset_sim()
+        self.goal = self._sample_goal().copy()
+        obs = self._get_obs()
+        if self.render_mode == "human":
+            self.render()
+
+        return obs, {}

2a30755310eb476a2ee1a2781a2aba7a11c9b915
Edward Hu <husedward@gmail.com>
2023-03-21 17:27:42 -0400

    robot action space for FetchPushQuad

--- gymnasium_robotics/__init__.py
+++ gymnasium_robotics/__init__.py
@@ -77,12 +77,26 @@ def register_robotics_envs():
         )
 
         register(
-            id=f"FetchPushQuadHard-v0",
+            id=f"ObjectPushQuadHard-v0",
             entry_point="gymnasium_robotics.envs.fetch.push_quad:MujocoFetchPushQuadHardEnv",
             kwargs={
                 "camera_names": ["camera_q1", "camera_q2", "camera_q3", "camera_q4","camera_overhead", "camera_under"],
                 "reward_type": "dense",
                 "action_space_type": "object",
+                "render_mode": "rgb_array",
+                "width": 32,
+                "height": 32,
+            },
+            max_episode_steps=50,
+        )
+
+        register(
+            id=f"FetchPushQuadHard-v0",
+            entry_point="gymnasium_robotics.envs.fetch.push_quad:MujocoFetchPushQuadHardEnv",
+            kwargs={
+                "camera_names": ["camera_q1", "camera_q2", "camera_q3", "camera_q4","camera_overhead", "camera_under"],
+                "reward_type": "dense",
+                "action_space_type": "robot",
                 "render_mode": "rgb_array",
                 "width": 32,
                 "height": 32,

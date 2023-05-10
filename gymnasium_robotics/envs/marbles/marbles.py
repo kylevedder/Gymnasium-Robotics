19aec584bb4621f6a7b9a3e8a30aac3c7b8a62f7
Edward Hu <husedward@gmail.com>
2023-05-10 18:46:45 -0400

    marbles camera version

--- gymnasium_robotics/envs/marbles/marbles.py
+++ gymnasium_robotics/envs/marbles/marbles.py
@@ -1,10 +1,14 @@
-from os import path 
-from typing import Optional
+import functools
+import itertools
+from os import path
 
+import mujoco
 import numpy as np
 from gymnasium import spaces
 from gymnasium.envs.mujoco.mujoco_env import MujocoEnv
+
 from gymnasium_robotics.utils import mujoco_utils
+
 
 class SweepMarblesEnv(MujocoEnv):
 
@@ -26,7 +30,7 @@ class SweepMarblesEnv(MujocoEnv):
             reward_type (str, optional): If the reward function is dense or sparse. Defaults to "dense".
         """
         assert observation_type in {"PO", "FO"}
-        assert pixel_ob in {"state", "pixels", "depth"}
+        assert pixel_ob in {"state", "camera", "pixels", "depth"}
         assert reward_type in {"dense", "sparse"}
 
         self.reward_scale = 10 
@@ -40,6 +44,10 @@ class SweepMarblesEnv(MujocoEnv):
             if self.pixel_ob == "state":
                 observation_space = spaces.Box(
                     low=-np.inf, high=np.inf, shape=(7,), dtype=np.float64
+                )
+            elif self.pixel_ob == "camera":
+                observation_space = spaces.Box(
+                    low=-np.inf, high=np.inf, shape=(8,), dtype=np.float64
                 )
             elif self.pixel_ob == "pixels":
                 _observation_space = {
@@ -71,6 +79,10 @@ class SweepMarblesEnv(MujocoEnv):
             if self.pixel_ob == "state":
                 observation_space = spaces.Box(
                     low=-np.inf, high=np.inf, shape=(13,), dtype=np.float64
+                )
+            elif self.pixel_ob == "camera":
+                observation_space = spaces.Box(
+                    low=-np.inf, high=np.inf, shape=(16,), dtype=np.float64
                 )
             elif self.pixel_ob == "pixels":
                 _observation_space = {
@@ -129,6 +141,10 @@ class SweepMarblesEnv(MujocoEnv):
         qpos[3] += marble_y_noise
         self.set_state(qpos, self.init_qvel)
         self.initial_marble_pos = mujoco_utils.get_site_xpos(self.model, self.data, "object0").copy()
+        if self.pixel_ob == "camera":
+            self.initial_marble_corners = self._get_marble_corners("robot_camera")
+            self.prev_marble_corners = self._get_marble_corners("overhead_camera")
+
         obs, _ = self._get_obs()
         return obs
 
@@ -136,6 +152,8 @@ class SweepMarblesEnv(MujocoEnv):
         action = np.clip(action, -1.0, 1.0)
         self.do_simulation(action, self.frame_skip)
         obs, info = self._get_obs()
+        if self.pixel_ob == "camera" and self.observation_type == "FO":
+            self.prev_marble_corners = self._get_marble_corners("overhead_camera")
         # reward function should check if the marble is in the bin.
         # if the marble xy position is within the bin and the z position is within the bin height
         marble_pos = mujoco_utils.get_site_xpos(self.model, self.data, "object0").copy()
@@ -165,6 +183,9 @@ class SweepMarblesEnv(MujocoEnv):
         State:
             PO: 7 dimensional. Dims 1-3 is the initial marble pos. Dims 4-7 are sweeper xy, xy vel.
             FO: 13 dimensional. PO obs + marble xyz pos, xyz vel. 
+        Camera:
+            PO: 8 dimensional. Dims 1-4 are the initial marble corners. Dims 5-8 is the sweeper state.
+            FO: 16 dimensional. PO obs + marble xy pos, xy vel.
         Pixels:
             PO: TODO: decide PO and FO camera viewpoints.
             FO:
@@ -185,6 +206,15 @@ class SweepMarblesEnv(MujocoEnv):
                 marble_vel = mujoco_utils.get_joint_qvel(self.model, self.data, "object0:joint")[:3]
                 obs = np.concatenate([self.initial_marble_pos, sweeper_state, marble_pos, marble_vel])
                 return obs, {}
+        elif self.pixel_ob == "camera":
+            if self.observation_type == "PO":
+                obs = np.concatenate([sweeper_state, self.initial_marble_corners])
+                return obs, {}
+            elif self.observation_type == "FO":
+                marble_corners = self._get_marble_corners("overhead_camera")
+                marble_vel = marble_corners - self.prev_marble_corners
+                obs = np.concatenate([sweeper_state, self.initial_marble_corners, marble_corners, marble_vel])
+                return obs, {}
         elif self.pixel_ob == "pixels":
             obs = {"sweeper_state": sweeper_state, "initial_view": self.initial_view}
             cameras = ["robot_camera"]
@@ -202,29 +232,86 @@ class SweepMarblesEnv(MujocoEnv):
                 obs[c] = self.mujoco_renderer.render("depth_array", camera_name=c)[..., None]
             return obs, {}
 
+    def _get_marble_corners(self, camera):
+        # get object position
+        box_pos = self.data.geom_xpos[self.model.geom('object0').id]
+        # Get the world coordinates of the box corners
+        box_pos = self.data.geom_xpos[self.model.geom('object0').id]
+        box_mat = self.data.geom_xmat[self.model.geom('object0').id].reshape(3, 3)
+        box_size = self.model.geom_size[self.model.geom('object0').id]
+        offsets = np.array([-1, 1]) * box_size[:, None]
+        xyz_local = np.stack(list(itertools.product(*offsets))).T
+        xyz_global = box_pos[:, None] + box_mat @ xyz_local
+        # Camera matrices multiply homogenous [x, y, z, 1] vectors.
+        corners_homogeneous = np.ones((4, xyz_global.shape[1]), dtype=float)
+        corners_homogeneous[:3, :] = xyz_global
+
+        # Get the camera matrix.
+        m = self._compute_camera_matrix(camera)
+
+        # Project world coordinates into pixel space. See:
+        # https://en.wikipedia.org/wiki/3D_projection#Mathematical_formula
+        xs, ys, s = m @ corners_homogeneous
+        # x and y are in the pixel coordinate system.
+        x = xs / s
+        y = ys / s
+        marble_corners = np.array([x[0], y[0], x[-1], y[-1]])
+        return marble_corners
+
+    @functools.lru_cache(maxsize=128)
+    def _compute_camera_matrix(self, camera_name):
+        """Returns the 3x4 camera matrix."""
+        camera = self.data.camera(camera_name)
+        camera_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_CAMERA, camera_name)
+        pos = camera.xpos.copy()
+        rot = camera.xmat.reshape(3,3).T.copy()
+        fov = self.model.cam_fovy[camera_id]
+
+        # Translation matrix (4x4).
+        translation = np.eye(4)
+        translation[0:3, 3] = -pos
+
+        # Rotation matrix (4x4).
+        rotation = np.eye(4)
+        rotation[0:3, 0:3] = rot
+
+        # Focal transformation matrix (3x4).
+        focal_scaling = (1./np.tan(np.deg2rad(fov)/2)) * self.height / 2.0
+        focal = np.diag([-focal_scaling, focal_scaling, 1.0, 0])[0:3, :]
+
+        # Image matrix (3x3).
+        image = np.eye(3)
+        image[0, 2] = (self.width - 1) / 2.0
+        image[1, 2] = (self.height - 1) / 2.0
+        return image @ focal @ rotation @ translation
+
 
 if __name__ == "__main__":
     import gymnasium
     import imageio
-    env = gymnasium.make("SweepMarblesFODepthDense-v0", render_mode="rgb_array")
-    obs, _ = env.reset()
-    def depth_to_img(depth):
-        # logic taken from mujoco colab tutorial
-        # Shift nearest values to the origin.
-        depth -= depth.min()
-        # Scale by 2 mean distances of near rays.
-        depth /= 2*depth[depth <= 1].mean()
-        # Scale to [0, 255]
-        pixels = 255*np.clip(depth, 0, 1)
-        return pixels.astype(np.uint8).squeeze()
-
-    for k,v in obs.items():
-        if k != "sweeper_state":
-            img = depth_to_img(v)
-            imageio.imwrite(f"{k}_depth.png", img)
+    env = gymnasium.make("SweepMarblesPOCameraDense-v0", render_mode="rgb_array")
+    obs = env.reset()
 
 
-    import ipdb; ipdb.set_trace()
+
+    # env = gymnasium.make("SweepMarblesFODepthDense-v0", render_mode="rgb_array")
+    # obs, _ = env.reset()
+    # def depth_to_img(depth):
+    #     # logic taken from mujoco colab tutorial
+    #     # Shift nearest values to the origin.
+    #     depth -= depth.min()
+    #     # Scale by 2 mean distances of near rays.
+    #     depth /= 2*depth[depth <= 1].mean()
+    #     # Scale to [0, 255]
+    #     pixels = 255*np.clip(depth, 0, 1)
+    #     return pixels.astype(np.uint8).squeeze()
+
+    # for k,v in obs.items():
+    #     if k != "sweeper_state":
+    #         img = depth_to_img(v)
+    #         imageio.imwrite(f"{k}_depth.png", img)
+
+
     # env = gymnasium.make("SweepMarblesFOPixelsDense-v0", render_mode="rgb_array")
     # import imageio
     # for i in range(1):

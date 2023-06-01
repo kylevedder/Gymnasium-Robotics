67cfc8511655c2b06dc0b2eb28b46d26c294574e
James Springer <js@barrelproofapps.com>
2023-06-01 17:12:12 -0400

    Set goals on opposite sides

--- gymnasium_robotics/envs/fetch/occluded_pick_place_recessed_2goal.py
+++ gymnasium_robotics/envs/fetch/occluded_pick_place_recessed_2goal.py
@@ -15,7 +15,10 @@ MODEL_XML_PATH = os.path.join("fetch", "occluded_pick_place_recessed_2goal.xml")
 class FetchOccludedPickPlaceRecessed2GoalEnv(FetchOccludedPickPlaceRecessedEnv):
     def __init__(self, **kwargs):
         super().__init__(model_xml_path=MODEL_XML_PATH, **kwargs)
-        self.goal_indicator_offset = np.array([0,0.075,-0.075])
+        self.goal_indicator_offset = np.array([
+            [0.0,-0.075,0.075],  # goal0 offset
+            [0.0,0.075,0.075]    # goal1 offset
+        ])  
 
     def _sample_goal(self):  # Randomly select between goal 0 and 1 and place indicator
         goal_num = np.random.randint(2)
@@ -23,14 +26,14 @@ class FetchOccludedPickPlaceRecessed2GoalEnv(FetchOccludedPickPlaceRecessedEnv):
         
         # Move indicator
         body_id = self._mujoco.mj_name2id(self.model, self._mujoco.mjtObj.mjOBJ_BODY, "goal_indicator")
-        self.model.body_pos[body_id] = goal_pos - self.goal_indicator_offset
+        self.model.body_pos[body_id] = goal_pos + self.goal_indicator_offset[goal_num]
         self._mujoco.mj_forward(self.model, self.data)
         return goal_pos
 
 if __name__ == "__main__":
     # import gymnasium
     # env = gymnasium.make("FOOccludedPickPlaceRecessed-v0")
-    env = FetchOccludedPickPlaceRecessed2GoalEnv(camera_names=["external_camera_0", "behind_camera"], reward_type="dense", render_mode="depth_array", width=64, height=64)
+    env = FetchOccludedPickPlaceRecessed2GoalEnv(camera_names=["external_camera_0", "behind_camera"], reward_type="dense", render_mode="human", width=64, height=64)
     obs, _ = env.reset()
     
     # Push block in goal0
